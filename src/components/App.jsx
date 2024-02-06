2ef0abc969cffea85fb00380b3b2d0f8e464c3e4
junayed-code <junayed.code@gmail.com>
2024-02-06 13:03:18 +0600

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,40 @@
+import Steps from "./Steps";
+import Main from "./composition/Main";
+
+function App() {
+  return (
+    <Main>
+      <div className="max-w-[920px] bg-white rounded-xl mx-auto p-5 flex gap-x-8">
+        <Steps />
+        <div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
+          culpa maxime, est quis quod minus numquam pariatur distinctio commodi
+          quidem atque necessitatibus eaque hic dicta dolores nisi nulla
+          explicabo eos ex sed illum? Pariatur repudiandae delectus animi
+          asperiores! Voluptatibus eligendi culpa recusandae veniam
+          reprehenderit libero deleniti delectus, ipsa aliquam dicta
+          consequuntur totam voluptas, neque, nostrum quas voluptates labore
+          saepe esse! Ex eum totam eligendi doloribus debitis, unde repudiandae
+          sint aperiam consequatur fuga consequuntur eveniet quidem optio dicta
+          sed cum dolorem laudantium autem at dolor modi explicabo. Aut
+          molestias impedit suscipit reprehenderit culpa! Quasi est repellat
+          officia a temporibus placeat inventore autem, quidem eum dolorum
+          aliquid voluptatibus repellendus consequuntur vero beatae ipsa
+          praesentium libero at animi quas asperiores debitis. Animi aut error
+          officiis quo, soluta saepe repellendus magnam est, illo eveniet
+          debitis iste. Accusantium similique blanditiis provident iste rerum,
+          ipsam sed vel totam! Obcaecati vero cumque natus asperiores amet
+          laboriosam corrupti animi deleniti accusamus ipsum vel incidunt
+          reiciendis inventore soluta eius, expedita aut odio at doloribus,
+          excepturi, officia saepe maiores? Temporibus iusto architecto quos vel
+          dolorum suscipit sequi similique nihil adipisci et consequatur sit,
+          voluptatibus, odit deleniti molestias tempore quisquam repellat
+          veritatis quae in perspiciatis expedita quo? At reprehenderit officiis
+          quo!
+        </div>
+      </div>
+    </Main>
+  );
+}
+
+export default App;
---
+++ src/components/composition/Main.jsx
@@ -0,0 +1,9 @@
+function Main({ children }) {
+  return (
+    <main className="flex justify-center items-center min-h-screen">
+      {children}
+    </main>
+  );
+}
+
+export default Main;
