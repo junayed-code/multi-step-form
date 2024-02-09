a5c45ab79e26ca42275579e77459b03371ce2804
junayed-code <junayed.code@gmail.com>
2024-02-09 19:46:52 +0600

    Update App component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,38 +1,59 @@
-import Steps from "./Steps";
+import AddOne from "./AddOne";
+import Sidebar from "./Sidebar";
+import Summary from "./Summary";
+import UserInfo from "./UserInfo";
+import SelectPlan from "./SelectPlan";
+import Confirmation from "./Confirmation";
+import NavigationButtons from "./NavigationButtons";
 import Main from "./composition/Main";
+import Container from "./composition/Container";
+import SidebarContainer from "./composition/SidebarContainer";
+
+import { useManageState } from "../hooks/useManageState";
 
 function App() {
+  const [state, dispatch] = useManageState();
+  const { step, userinfo, planOption, membership, optionalServices } = state;
+
   return (
     <Main>
-      <div className="max-w-[920px] bg-white rounded-xl mx-auto p-5 flex gap-x-8">
-        <Steps />
-        <div>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
-          culpa maxime, est quis quod minus numquam pariatur distinctio commodi
-          quidem atque necessitatibus eaque hic dicta dolores nisi nulla
-          explicabo eos ex sed illum? Pariatur repudiandae delectus animi
-          asperiores! Voluptatibus eligendi culpa recusandae veniam
-          reprehenderit libero deleniti delectus, ipsa aliquam dicta
-          consequuntur totam voluptas, neque, nostrum quas voluptates labore
-          saepe esse! Ex eum totam eligendi doloribus debitis, unde repudiandae
-          sint aperiam consequatur fuga consequuntur eveniet quidem optio dicta
-          sed cum dolorem laudantium autem at dolor modi explicabo. Aut
-          molestias impedit suscipit reprehenderit culpa! Quasi est repellat
-          officia a temporibus placeat inventore autem, quidem eum dolorum
-          aliquid voluptatibus repellendus consequuntur vero beatae ipsa
-          praesentium libero at animi quas asperiores debitis. Animi aut error
-          officiis quo, soluta saepe repellendus magnam est, illo eveniet
-          debitis iste. Accusantium similique blanditiis provident iste rerum,
-          ipsam sed vel totam! Obcaecati vero cumque natus asperiores amet
-          laboriosam corrupti animi deleniti accusamus ipsum vel incidunt
-          reiciendis inventore soluta eius, expedita aut odio at doloribus,
-          excepturi, officia saepe maiores? Temporibus iusto architecto quos vel
-          dolorum suscipit sequi similique nihil adipisci et consequatur sit,
-          voluptatibus, odit deleniti molestias tempore quisquam repellat
-          veritatis quae in perspiciatis expedita quo? At reprehenderit officiis
-          quo!
-        </div>
-      </div>
+      <Container>
+        <Sidebar index={state.index} />
+        <SidebarContainer>
+          {step === "user-info" && (
+            <UserInfo
+              userinfo={userinfo}
+              dispatch={dispatch}
+              errors={state.errors}
+              touched={state.touched}
+            />
+          )}
+          {step === "select-plan" && (
+            <SelectPlan
+              dispatch={dispatch}
+              planOption={planOption}
+              membership={membership}
+            />
+          )}
+          {step === "add-one" && (
+            <AddOne
+              dispatch={dispatch}
+              planOption={planOption}
+              optionalServices={optionalServices}
+            />
+          )}
+          {step === "summary" && (
+            <Summary
+              dispatch={dispatch}
+              planOption={planOption}
+              membership={membership}
+              optionalServices={optionalServices}
+            />
+          )}
+          {step === "confirmation" && <Confirmation />}
+          <NavigationButtons dispatch={dispatch} step={step} />
+        </SidebarContainer>
+      </Container>
     </Main>
   );
 }
